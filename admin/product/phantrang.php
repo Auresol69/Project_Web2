fa7f26930c5b5cf0e3b9e34f97e0f3b131844dd9
khangpdm <phamdangminhkhang2005@gmail.com>
2025-04-24 17:34:49 +0700

    Update phantrang.php

--- admin/product/phantrang.php
+++ admin/product/phantrang.php
@@ -13,24 +13,24 @@ class Pagination {
 
     public function createUrl($page) {
         $queryParams = $_GET;
-        $queryParams['page'] = $page;
+        $queryParams['pages'] = $page;
         $queryParams['per_page'] = $this->item_per_page;
         return '?' . http_build_query($queryParams);
     }
 
     public function render() {
         $output = '<div id="pagination">';
-
+    
         // First page link
         if ($this->current_page > 3) {
             $output .= '<a class="page-item" href="' . $this->createUrl(1) . '">First</a>';
         }
-
+    
         // Previous page link
         if ($this->current_page > 1) {
             $output .= '<a class="page-item" href="' . $this->createUrl($this->current_page - 1) . '">Prev</a>';
         }
-
+    
         // Page number links
         for ($num = 1; $num <= $this->total_pages; $num++) {
             if ($num != $this->current_page) {
@@ -41,19 +41,19 @@ class Pagination {
                 $output .= '<strong class="current-page page-item">' . $num . '</strong>';
             }
         }
-
+    
         // Next page link
         if ($this->current_page < $this->total_pages) {
             $output .= '<a class="page-item" href="' . $this->createUrl($this->current_page + 1) . '">Next</a>';
         }
-
+    
         // Last page link
         if ($this->current_page < $this->total_pages - 3) {
             $output .= '<a class="page-item" href="' . $this->createUrl($this->total_pages) . '">Last</a>';
         }
-
+    
         $output .= '</div>';
-
+    
         return $output;
     }
 }
