af7841d1368ec547addb3d8c4a7e37c0d9ac72e0
khangpdm <phamdangminhkhang2005@gmail.com>
2025-05-05 09:19:00 +0700

    Add files via upload

--- admin/database/database.php
+++ admin/database/database.php
@@ -3,7 +3,7 @@
     $db_user = "root";
     $db_pass = "";
     $db_host = "localhost";
-    $db_port = 3306; // Define default MySQL port
+
 
     // Kết nối đến cơ sở dữ liệu MySQL
     $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name, $db_port);
